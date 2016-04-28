6bf682abddf7723da62df57d9eb3f7e4fd3f5804
whereitat <erik.hallkvist@gmail.com>
2016-04-28 21:18:32 +0200

    Player with funnyhatbonus

---
+++ WorldGame/WorldGame/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldGame
+{
+    class Player
+    {
+        private string name;
+        private int level;
+        private double hatBonus;
+        private int xp;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int XP
+        {
+            get
+            {
+                return xp;
+            }
+
+            set
+            {
+                xp = value;
+            }
+        }
+        public double HatBonus
+        {
+            set { this.hatBonus = value};
+            get { return hatBonus; }
+        } 
+    }
+}

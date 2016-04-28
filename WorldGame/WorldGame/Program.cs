f8b2e210247ec6f6c4e260e3ff187a655cbe314c
marcus <marcus.havrell@gmail.com>
2016-04-28 20:32:01 +0200

    a

--- WorldGame/WorldGame/Program.cs
+++ WorldGame/WorldGame/Program.cs
@@ -22,6 +22,7 @@ namespace WorldGame
         public Model()
         {
             
+            public 
         }
 
     }
@@ -30,14 +31,32 @@ namespace WorldGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! You must build industries and do science to get further out in space and find new planets.");
+            bool check = true;
+
+            Console.WriteLine("Hello! You are the only surivor of a spaceship crash on an unknown planet. To survive you must do science and invent new things.");
             Console.WriteLine("Do you wish to continue?");
 
-            if(Console.ReadLine() == "yes")
+            
+            Console.ReadKey();
+            if(Console.ReadLine() == "no")
             {
-                Console.WriteLine("Good. ")
+                Console.WriteLine("fuck you then");
+            } else
+            {
+                Console.WriteLine("good for you");
             }
 
+            Console.WriteLine("What is your name?");
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
